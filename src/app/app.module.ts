f02579844fabb7e7e8bf78c78e6c7cad71407cb8
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-11-29 16:46:46 +0100

    directives: how directives work - task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoldDirective } from './bold.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoldDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/bold.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appBold]'
+})
+export class BoldDirective {
+
+  constructor(el: ElementRef) {
+    el.nativeElement.style.fontWeight = 'bold';
+  }
+}

1516af3bf2c429284c7715dd0ad0d17cfcf8572a
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-12-18 16:36:21 +0100

    attribute-directives-intro-1

--- src/app/directives/avatar.directive.ts
+++ src/app/directives/avatar.directive.ts
@@ -10,6 +10,7 @@ export class AvatarDirective implements OnInit {
 
   ngOnInit() {
     const size = '64px';
+    // setting width & height to 64px via instance of ElementRef
     this.elementRef.nativeElement.style.width = size;
     this.elementRef.nativeElement.style.height = size;
   }

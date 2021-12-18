1755cda0e1876f72cad769efc26262a94c4f1ae9
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-12-18 17:20:37 +0100

    attribute-directives-own-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  // TODO receive the event and log message in the console
+  product: Product = {
+    name: 'Product 1',
+    id: 'p-1',
+    description: 'Product 1 description',
+    image: '/assets/p1.png',
+    price: 110,
+  };
 
+  product2: Product = {
+    name: 'Product 2',
+    id: 'p-2',
+    description: 'Product 2 description',
+    image: '/assets/p2.png',
+    price: 90,
+  };
+
+  product3: Product = {
+    name: 'Product 3',
+    id: 'p-3',
+    description: 'Product 3 description',
+    image: '/assets/p3.png',
+    price: 150,
+  };
 }
---
+++ src/app/directives/image.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, Input, OnChanges, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Directive({
+  selector: '[appImage]'
+})
+export class ImageDirective implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}

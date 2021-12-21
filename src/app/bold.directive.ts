76a88ff40c348d8b7136d2c59fae0bbbcd9c9fca
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-12-21 14:35:14 +0100

    how-directives-work-live-1

--- src/app/bold.directive.ts
+++ src/app/bold.directive.ts
@@ -6,6 +6,7 @@ import { Directive, ElementRef } from '@angular/core';
 export class BoldDirective {
 
   constructor(el: ElementRef) {
+    // using ElementRef instance to apply style changes:
     el.nativeElement.style.fontWeight = 'bold';
   }
 }

ba027ca2689694e09f3bd28c865c1b5b921c2674
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-12-21 21:11:12 +0100

    directives-ng-class-live-1

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -2,11 +2,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  template: `
-    <button>Hello</button>`,
-  styleUrls: ['./button.component.scss']
+  template: `<button [ngClass]="buttonClass">Hello</button>`,
 })
 export class ButtonComponent {
-  @Input() color?: 'primary' | 'secondary';
-  @Input() warn?: boolean;
+  @Input() active: boolean = true;
+
+  get buttonClass(): string {
+    if(this.active){
+      return 'app-button button-active';
+    } else{
+      return 'app-button';
+    }
+  }
 }

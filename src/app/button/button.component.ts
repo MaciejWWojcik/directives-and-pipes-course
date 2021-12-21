a0cc7dd0e9711b27bbed79454131c0ce6f3ad1dd
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-12-21 21:15:42 +0100

    comments

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  template: `
-    <button>Hello</button>`,
+  // TODO add NgClass directive and pass there correct class-names based on values of the component's properties
+  template: `<button>Hello</button>`,
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {

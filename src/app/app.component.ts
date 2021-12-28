efe3243f9233da21fc33363285b6ed3ad0fd9d98
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-12-28 09:18:23 +0100

    solution 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  red: number = 0;
-  green: number = 0;
-  blue: number = 0;
+  red: number = 200;
+  green: number = 120;
+  blue: number = 240;
+
+  get redStyle() {
+    return { backgroundColor: `rgb(${this.red},0,0)` };
+  }
+
+  get greenStyle() {
+    return { backgroundColor: `rgb(0,${this.green},0)` };
+  }
+
+  get blueStyle() {
+    return { backgroundColor: `rgb(0,0,${this.blue})` };
+  }
+
+  get resultStyle() {
+    return { backgroundColor: `rgb(${this.red},${this.green},${this.blue})` };
+  }
 
   updateRed(event: Event) {
     this.red = +(<HTMLInputElement>event.target).value;

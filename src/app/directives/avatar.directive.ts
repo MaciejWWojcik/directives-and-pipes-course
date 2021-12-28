d738e99d635a7c77f0aa3ac0956ced7f4061b9dd
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-12-28 11:02:09 +0100

    solution 1

--- src/app/directives/avatar.directive.ts
+++ src/app/directives/avatar.directive.ts
@@ -1,16 +1,18 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[appAvatar]' // TODO update selector so the directive applies only to images
+  selector: 'img[appAvatar]'
 })
 export class AvatarDirective implements OnInit {
 
-  constructor() {
+  constructor(private elementRef: ElementRef) {
   }
 
   ngOnInit() {
-    // TODO set size to 72px x 72px
-    // TODO add yellow, 1px border
-    // TODO round the element, so it is in the shape of the circle
+    const size = '72px';
+    this.elementRef.nativeElement.style.width = size;
+    this.elementRef.nativeElement.style.height = size;
+    this.elementRef.nativeElement.style.border = `1px solid gold`;
+    this.elementRef.nativeElement.style.borderRadius = size;
   }
 }

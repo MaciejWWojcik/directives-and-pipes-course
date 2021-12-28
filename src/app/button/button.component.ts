855cc2c53a2d7c648ab236788cfc4a7bcf582781
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-12-28 08:59:25 +0100

    solution 1

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -2,11 +2,30 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  // TODO add NgClass directive and pass there correct class-names based on values of the component's properties
-  template: `<button>Hello</button>`,
+  template: `
+    <button [ngClass]="buttonClass">Hello</button>`,
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
   @Input() color?: 'primary' | 'secondary';
   @Input() warn?: boolean;
+
+  // getter bound to ngClass in the template
+  get buttonClass(): string {
+    let classNames = 'app-button ';
+
+    if (this.warn) {
+      classNames += 'warn-button';
+    } else {
+      // if it's not in warn mode then apply classes according to color mode
+      if (this.color === 'primary') {
+        classNames += 'primary-button';
+      }
+      if (this.color === 'secondary') {
+        classNames += 'secondary-button';
+      }
+    }
+
+    return classNames;
+  }
 }

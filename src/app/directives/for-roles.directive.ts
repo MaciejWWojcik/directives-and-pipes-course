aece038cd5ec236c8f99d70f5d1e63ed1b6682a5
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-12-28 15:07:34 +0100

    solution 1

--- src/app/directives/for-roles.directive.ts
+++ src/app/directives/for-roles.directive.ts
@@ -6,17 +6,20 @@ import {
   Input,
   Output,
   TemplateRef,
-  ViewContainerRef
+  ViewContainerRef, OnChanges
 } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { UserRole } from '../models/user-role';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[appForRoles]'
 })
-export class ForRolesDirective implements OnInit {
+export class ForRolesDirective implements OnChanges {
 
   @Input('appForRoles') allowedRoles!: (UserRole | string)[];
+
+  private subscription: Subscription = new Subscription();
 
   constructor(
     private userService: UserService,
@@ -25,14 +28,22 @@ export class ForRolesDirective implements OnInit {
   ) {
   }
 
-  ngOnInit() {
-    this.userService.userRole$.subscribe(role => {
+  ngOnChanges() {
+    // clear any existing subscriptions
+    this.subscription.unsubscribe();
+    this.subscription = new Subscription();
+
+    // react on role changes to render or hide elements
+    const sub = this.userService.userRole$.subscribe(role => {
       if (this.allowedRoles.includes(role)) {
         this.viewContainerRef.clear();
         this.viewContainerRef.createEmbeddedView(this.templateRef);
       } else {
         this.viewContainerRef.clear();
       }
-    })
+    });
+
+    // add current subscription to the list
+    this.subscription.add(sub);
   }
 }

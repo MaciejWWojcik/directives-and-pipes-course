adaf3d89e603e9cd8001c7543bb12799c4803472
Maciej Wojcik <maciejwwojcik@gmail.com>
2021-12-28 13:52:47 +0100

    solution 1

--- src/app/directives/image.directive.ts
+++ src/app/directives/image.directive.ts
@@ -1,13 +1,18 @@
 import { Directive, ElementRef, Input, OnChanges, OnInit, Output, EventEmitter } from '@angular/core';
+import { Product } from '../models/product';
 
 @Directive({
-  selector: '[appImage]'
+  selector: 'img[appImage]'
 })
 export class ImageDirective implements OnInit {
+  @Input('appImage') product!: Product;
 
-  constructor() {
+  constructor(private elementRef: ElementRef) {
   }
 
   ngOnInit() {
+    this.elementRef.nativeElement.setAttribute('loading', 'lazy');
+    this.elementRef.nativeElement.setAttribute('alt', `Image shows ${this.product.name}`);
+    this.elementRef.nativeElement.setAttribute('src', this.product.image);
   }
 }

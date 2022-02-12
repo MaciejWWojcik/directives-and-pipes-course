e9517b668f94ccf9cbe38d63ce58ba4a18054484
Maciej Wojcik <maciejwwojcik@gmail.com>
2022-02-12 10:58:55 +0100

    task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Account, User } from './user';
-import { Observable } from 'rxjs';
-import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
 
-  // TODO: create a property, to keep Observable of data
-
-  constructor(
-    private userService: UserService,
-  ) {
-  }
-
-  ngOnInit() {
-    // TODO get user from the service (as Observable)
-  }
-
+  dateObject = new Date('05/15/2022');
 }
